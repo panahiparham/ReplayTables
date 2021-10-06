f53019b706fc0ff75a51ee928130d955ff0b1b56
Andy Patterson <andnpatterson@gmail.com>
2021-10-06 12:03:42 -0600

    Depend on correct version of numpy

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*']),
     install_requires=[
-        'numpy>=1.21.0',
+        'numpy<1.21.0,>=1.17',
         'numba>=0.52.0',
     ],
-    version='0.0.0',
+    version='0.0.1',
     license='MIT',
     description='A simple replay buffer implementation in python for sampling n-step trajectories',
     long_description='todo',
