cd0e77cfc5a81c32f1a5713a8f357d4f9b7bf3b4
andy <andnpatterson@gmail.com>
2022-04-17 12:32:42 -0600

    ci: switch to modern build system

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
-    name='ReplayTables',
+    name='ReplayTables-andnp',
     url='https://github.com/andnp/ReplayTables.git',
     author='Andy Patterson',
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*']),
+    version='0.0.2',
+    license='MIT',
+    description='A simple replay buffer implementation in python for sampling n-step trajectories',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
     install_requires=[
         'numpy>=1.21.0',
         'numba>=0.55.0',
     ],
-    version='0.0.2',
-    license='MIT',
-    description='A simple replay buffer implementation in python for sampling n-step trajectories',
-    long_description='todo',
+    extras_require={},
 )
