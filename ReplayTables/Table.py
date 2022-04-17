d6f496511fb0daec8ee691fab69513fa1a4b28c9
andy <andnpatterson@gmail.com>
2022-04-17 12:33:09 -0600

    feat: use faster numba settings

--- ReplayTables/Table.py
+++ ReplayTables/Table.py
@@ -1,8 +1,8 @@
 import numpy as np
 import numpy.typing as npt
 from typing import Dict, Iterable, List, Sequence, Tuple, TypedDict
-from numba import njit
 from ReplayTables.RandDict import RandDict
+from ReplayTables._utils.jit import try2jit
 
 class _ColumnDefReq(TypedDict):
     name: str
@@ -224,12 +224,12 @@ class View:
         for key in keys:
             del self._refs[key]
 
-@njit(cache=True)
+@try2jit
 def rotatedSequence(lo: int, hi: int, mod: int) -> np.ndarray:
     seq = np.arange(lo, hi, dtype=np.int64)
     return seq % mod
 
-@njit(cache=True)
+@try2jit
 def padded(arr: np.ndarray, size: int, mult: int, value: float = np.nan):
     s = int(np.ceil(size / mult) * mult)
     out = np.ones((s, ) + arr.shape[1:], dtype=arr.dtype) * value
