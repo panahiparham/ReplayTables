87b9c05bbb0ee74d3004a799e3ea15561b781bd7
andy <andnpatterson@gmail.com>
2022-11-07 08:55:56 -0700

    chore: remove outdated typing interfaces

--- typings/numba/experimental/__init__.pyi
+++
@@ -1,4 +0,0 @@
-from typing import TypeVar
-
-T = TypeVar('T')
-def jitclass(f: T) -> T: ...
--- typings/numba/typed/__init__.pyi
+++
@@ -1,4 +0,0 @@
-from typing import List as PyList
-
-class List(PyList):
-    def __init__(self, l: PyList): ...
