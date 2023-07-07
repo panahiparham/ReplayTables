afa125c276165d92f4ee3501c84cc914fbddcaef
andy <andnpatterson@gmail.com>
2023-07-07 14:23:17 -0600

    ci: update integration performance tracking

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,77 +1,46 @@
 import numpy as np
-from typing import Any, NamedTuple
 
 from ReplayTables.ReplayBuffer import ReplayBuffer
-from ReplayTables.LagBuffer import LagBuffer
-
-class Experience(NamedTuple):
-    s: Any
-    a: int
-    r: float | None
-    gamma: float
-    terminal: bool
-
-class Lagged(NamedTuple):
-    s: Any
-    a: Any
-    r: Any
-    gamma: Any
-    sp: Any
+from ReplayTables.interface import Timestep
 
 # ----------------
 # -- Benchmarks --
 # ----------------
 class TestBenchmarks:
     def test_1_step_loop(self, benchmark):
-        def rl_loop(lag: LagBuffer, buffer: ReplayBuffer, d):
+        def rl_loop(buffer: ReplayBuffer, d):
             for _ in range(100):
-                for exp in lag.add(d):
-                    l = Lagged(
-                        s=exp.s,
-                        a=exp.a,
-                        r=exp.r,
-                        gamma=exp.gamma,
-                        sp=exp.sp,
-                    )
-                    buffer.add(l)
+                buffer.add(d)
+                if buffer.size() > 1:
                     _ = buffer.sample(32)
 
         rng = np.random.default_rng(0)
-        lag = LagBuffer(1)
-        buffer = ReplayBuffer(30, Lagged, rng)
-        d = Experience(
-            s=np.zeros(50),
+        buffer = ReplayBuffer(30, 1, rng)
+        d = Timestep(
+            x=np.zeros(50),
             a=0,
             r=0.1,
             gamma=0.99,
             terminal=False,
         )
 
-        benchmark(rl_loop, lag, buffer, d)
+        benchmark(rl_loop, buffer, d)
 
     def test_3_step_loop(self, benchmark):
-        def rl_loop(lag: LagBuffer, buffer: ReplayBuffer, d):
+        def rl_loop(buffer: ReplayBuffer, d):
             for _ in range(100):
-                for exp in lag.add(d):
-                    l = Lagged(
-                        s=exp.s,
-                        a=exp.a,
-                        r=exp.r,
-                        gamma=exp.gamma,
-                        sp=exp.sp,
-                    )
-                    buffer.add(l)
+                buffer.add(d)
+                if buffer.size() > 1:
                     _ = buffer.sample(32)
 
         rng = np.random.default_rng(0)
-        lag = LagBuffer(3)
-        buffer = ReplayBuffer(30, Lagged, rng)
-        d = Experience(
-            s=np.zeros(50),
+        buffer = ReplayBuffer(30, 3, rng)
+        d = Timestep(
+            x=np.zeros(50),
             a=0,
             r=0.1,
             gamma=0.99,
             terminal=False,
         )
 
-        benchmark(rl_loop, lag, buffer, d)
+        benchmark(rl_loop, buffer, d)
