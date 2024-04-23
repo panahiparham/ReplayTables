e1ea9784a4daf60c7ff8224188dbba0b16039b01
Parham Panahi <panahiparham@gmail.com>
2024-04-23 00:06:06 -0600

    feat: added random_egress buffer

--- ReplayTables/registry.py
+++ ReplayTables/registry.py
@@ -5,6 +5,7 @@ from ReplayTables.ReplayBuffer import ReplayBuffer
 from ReplayTables.BackwardsReplay import BackwardsReplay, BackwardsReplayConfig
 from ReplayTables.PER import PrioritizedReplay, PERConfig
 from ReplayTables.PSER import PrioritizedSequenceReplay, PSERConfig
+from ReplayTables.PrototypeBuffer import RandomEgressBuffer
 
 def build_buffer(buffer_type: str, max_size: int, lag: int, rng: np.random.Generator, config: Dict[str, Any]) -> ReplayBuffer:
     buffer_type = buffer_type.lower()
@@ -26,5 +27,8 @@ def build_buffer(buffer_type: str, max_size: int, lag: int, rng: np.random.Gener
     elif buffer_type == 'pser':
         c = PSERConfig(**config)
         return PrioritizedSequenceReplay(max_size, lag, rng, c)
+
+    elif buffer_type == 'random_egress':
+        return RandomEgressBuffer(max_size, lag, rng)
 
     raise Exception('Unable to determine type of buffer')
